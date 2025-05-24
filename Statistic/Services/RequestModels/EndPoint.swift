22c26478732fd5a40638eecd4cafce3c37d074ac
Malik <timurkaev_malik@icloud.com>
2025-05-24 12:09:27 +0300

    RequestConstants were configured

---
+++ Statistic/Services/RequestModels/EndPoint.swift
@@ -0,0 +1,26 @@
+//
+//  EndPoint.swift
+//  Statistic
+//
+//  Created by Malik Timurkaev on 24.05.2025.
+//
+
+import Foundation
+
+enum EndPoint {
+    static let baseURL = "http://test.rikmasters.ru/api"
+    
+    case path(Path)
+    
+    var url: URL? {
+        switch self {
+        case .path(let path):
+            URL(string: EndPoint.baseURL + path.rawValue)
+        }
+    }
+    
+    enum Path: String {
+        case users = "/users"
+        case statistic = "/statistic"
+    }
+}
---
+++ Statistic/Services/RequestModels/HttpMethod.swift
@@ -0,0 +1,16 @@
+//
+//  HttpMethod.swift
+//  Statistic
+//
+//  Created by Malik Timurkaev on 24.05.2025.
+//
+
+
+import Foundation
+
+enum HttpMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+}
